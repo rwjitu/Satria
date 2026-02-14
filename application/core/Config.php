27c9d21696038752434d14afe0b4c24e8c8f564d
rwjitu <rwjitu@gmail.com>
2026-02-14 23:23:38 +0700

    Add files via upload

---
+++ application/core/Config.php
@@ -0,0 +1,25 @@
+<?php
+
+class Config
+{
+    // this is public to allow better Unit Testing
+    public static $config;
+
+    public static function get($key)
+    {
+        if (!self::$config) {
+
+
+            $config_file = '../application/config/config.' . Environment::get() . '.php';
+            //$config_file = '../application/config/config.' . $_SERVER['HTTP_HOST'] . '.php';
+
+            if (!file_exists($config_file)) {
+                return false;
+            }
+
+            self::$config = require $config_file;
+        }
+
+        return self::$config[$key];
+    }
+}
